7a39d32cc221c8498aa200150d0881bb7935f8bb
Sanshain <digital-mag@ya.ru>
2021-05-02 23:42:07 +0300

    make hints in templates

--- source/TSApp.tsx
+++ source/TSApp.tsx
@@ -7,10 +7,18 @@ const App: FunctionalComponent<IndexProps> = () => {
   return (
     <div>
       <main>
-        <h1>Welcome to Microsite!</h1>
+        <h1>Welcome to preact development!</h1>
         <p>
-          Ready to build something amazing? <a href="https://github.com/natemoo-re/microsite/tree/main/docs">Read the docs</a> to get started.
+          	Inspired by <a href="https://github.com/peterDijk/preact-typescript-rollup-starter">peterDijk template</a>			 
         </p>
+		  <p>
+		  		Ready to build something amazing? Read <a href="https://preactjs.com/guide/v10/typescript/#typescript-configuration">the docs to get started</a>
+		  </p>
+		  <h4>See elso:</h4>
+		  <ul>
+			  <li><a href="https://github.com/Kulak/preactRouterRollupExample/tree/master/src">js-ts boilerplate</a></li>
+			  <li><a href="https://github.com/raiyni/preact-rollup-typescript-babel/blob/master/.babelrc">babel-ts boilerplate</a></li>
+		  </ul>
       </main>
     </div>
   );
