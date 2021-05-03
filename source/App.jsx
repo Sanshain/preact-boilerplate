b9f7ec04e9ac0987b51170eccb75c831a0329577
Sanshain <digital-mag@ya.ru>
2021-05-03 15:56:17 +0300

    store on via crutch

--- source/App.jsx
+++ source/App.jsx
@@ -3,8 +3,9 @@ import { useState } from 'preact/hooks'
 
 import { styled, css, setup } from 'goober';
 
-import { useStoreon } from 'storeon/react' // or storeon/preact
+import { useStoreon } from 'storeon/preact' // or storeon/preact
 
+import { store } from "./store/state";
 
 setup(h);
 
@@ -20,13 +21,17 @@ const BtnClassName = css`
 const App = props => {
 
 	const [message] = useState('Preact App')
-	const { dispatch, count } = useStoreon('count')
+
+	// const { dispatch, count } = useStoreon('count')
+
 
 	return <>
 		<header />
 		<main class={BtnClassName}>
 			<h1 class='title'>{message}</h1>
-			<button onClick={() => store.dispatch('inc')}>{count}</button>
+			<button onClick={(e) => { store.dispatch('inc'); e.target.innerText=store.get().count;}}>
+				{store.get().count}
+			</button>
 		</main>
 		<Title>789</Title>
 	</>
--- source/index.js
+++ source/index.js
@@ -4,15 +4,7 @@ import { h, render } from 'preact'
 import App from './App'
 
 import { StoreContext } from 'storeon/react'
-import { store } from "./store/state";
 // import App from "./components/app";
 // import App from './TSApp'
 
-render(
-	<StoreContext.Provider value={store}>
-		<App />
-	</StoreContext.Provider>, 
-	document.getElementById('root')
-)
-
-// render(<App/>, document.getElementById('root'))
+render(<App/>, document.getElementById('root'))
