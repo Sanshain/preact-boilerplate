4a9373a2a02c82e618192314ac5f6ba80915be70
Alexandr <digital-mag@ya.ru>
2024-09-02 19:21:54 +0300

    works fine with sanshain version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
+// import { h } from "preact";
+import { styled } from 'goober';
+
+
 import './App.css';
 import './style.css'
+
+
+const Button = styled('button')`
+    background: dodgerblue;
+    color: white;  
+    transition: 0.1s;
+    margin: 1em 0;
+
+    &:focus,
+    &:hover {
+        padding: 1em;
+    }
+
+    .otherClass {
+        margin: 0;
+    }
+`;
+
+const Icon = styled('span')`
+    /* display: flex;
+    flex: 1; */
+    color: red;
+`;
+
+
 
 
 function App () {
     return (
         <div class="App">
-            <h1>Hello World!!!</h1>
+            <h1>Hello World!!</h1>
             <div className="header">111-111-12</div>
+            <Button onClick={() => alert(99)}>
+                click <Icon>me</Icon>
+            </Button>
         </div>
     );
 }
