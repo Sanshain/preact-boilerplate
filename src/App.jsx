12ff8c82e903204c47b9213a8bbc3e1944a80b9d
Alexandr <digital-mag@ya.ru>
2024-09-03 09:24:50 +0300

    goober works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
 import style from './style.module.css'
+import { styled, setup } from 'goober';
+
+
+
+const Icon = styled('span')`    
+    color: red;
+`;
+
+const Button = styled('button')`
+    background: dodgerblue;
+    color: white;
+    transition: .2s;
+    margin: 1em 0;
+    /* border: ${Math.random()}px solid white; */
+
+    &:focus,
+    &:hover {
+        padding: 1em;
+    }
+
+    .otherClass {
+        margin: 0;
+    }
+
+    ${Icon} {        
+        text-shadow: 0 0 5px white;
+    }
+`;
 
 
 function App () {
@@ -6,6 +34,7 @@ function App () {
         <div class={style.title}>
             <h1>Hello World!!!</h1>
             <div className={style.header}>111-111-122</div>
+            <Button>click <Icon>me</Icon></Button>
         </div>
     );
 }
