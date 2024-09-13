03c13344d3cc61cc9c1746ac311b37b602e6109b
Alexandr <digital-mag@ya.ru>
2024-09-13 18:58:03 +0300

    hmr on tsx works via esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,8 @@ import babel from '@rollup/plugin-babel';
 
 // import jsx from 'acorn-jsx';
 import typescript from '@rollup/plugin-typescript';
+import esbuild from 'rollup-plugin-esbuild'
+
 
 
 /** 
@@ -69,12 +71,37 @@ const config = {
             configFile: inDevelopment ? './.dev.babelrc' : './.babelrc'      // hmr           
         }),
 
-        typescript({
-            tsconfig: './tsconfig.json',            
-            exclude: [
-                './**/*.css'
-            ]
-        }),        
+        esbuild({
+            // All options are optional
+            include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+            exclude: /node_modules/, // default
+            sourceMap: true, // default
+            minify: process.env.NODE_ENV === 'production',
+            target: 'es2017', // default, or 'es20XX', 'esnext'
+            jsx: 'transform', // default, or 'preserve'
+            jsxFactory: 'React.createElement',
+            jsxFragment: 'React.Fragment',
+            // Like @rollup/plugin-replace
+            define: {
+                __VERSION__: '"x.y.z"',
+            },
+            tsconfig: 'tsconfig.json', // default
+            // Add extra loaders
+            loaders: {
+                // Add .json files support
+                // require @rollup/plugin-commonjs
+                '.json': 'json',
+                // Enable JSX in .js files too
+                '.js': 'jsx',
+            },
+        }),
+
+        // typescript({
+        //     tsconfig: './tsconfig.json',            
+        //     exclude: [
+        //         './**/*.css'
+        //     ]
+        // }),        
 
         node_resolve({
             extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css']
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import style from './style.module.css'
 
 
+const a: string = 'aaa';
+
 function App () {
     return (
         <div class={style.title}>
-            <h1>Hello World!!!</h1>
+            <h1>Hello World {a}!</h1>
             <div className={style.header}>111-111-122</div>
         </div>
     );
