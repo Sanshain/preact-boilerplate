15480287aca8413d1c9a694831b1122fb33702ef
Alexandr <digital-mag@ya.ru>
2024-09-14 09:41:13 +0300

    routes breaks hmr

---
+++ src/routes/home/index.tsx
@@ -0,0 +1,61 @@
+//@ts-check
+
+import { h } from 'preact';
+import { useRef } from 'preact/hooks';
+
+
+/// cssmodules
+
+import style from './style.css';
+
+
+/// goober
+
+import { Button } from "../../components/Button.jsx";
+
+
+/// linaria
+
+import Footer from '../../components/Footer.jsx'; 
+import { a } from '../../store/store';
+
+
+
+// import { storeHandler as userStore, users$ } from '../../store/store';
+// import { useStore } from '@nanostores/react';
+
+
+const b: string = '22';
+
+const Home = () => {
+	
+	// const users = useStore(users$);
+	const inputRef = useRef(null);
+
+	// const addUser = (e) => {
+	// 	const value = inputRef.current['value'];
+	// 	if (value.length) {
+	// 		userStore.appendUser(value);
+	// 		inputRef.current['value'] = ''	
+	// 	}
+	// };
+
+
+	return <div class={style.home}>
+
+		<h1>Home!! {a} {b} 1</h1>
+		<p>This is the Home component.</p>
+
+		{/* <input ref={inputRef} placeholder='enter user name' />
+		<button onClick={addUser}>add</button> ({users.length}) */}
+		
+        <Button>ckick me</Button>
+
+        <Footer>
+            <a href={"https://github.com/Sanshain/preact-boilerplate"} target={"_blank"}>Sanshain's templates</a>
+        </Footer>
+        
+	</div>
+};
+
+export default Home;
