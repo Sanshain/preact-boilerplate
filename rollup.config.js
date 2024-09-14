b97246c3922f7d667f01fc8a2aaf0f841821be30
Alexandr <digital-mag@ya.ru>
2024-09-14 13:32:44 +0300

    little clearing

--- rollup.config.js
+++ rollup.config.js
@@ -69,32 +69,20 @@ let config = {
     },
     plugins: [
 
-        /// used esbuild in development because of hmr failing on tsx files (original tsc for some reason cuts `RefreshSig` variables required for hmr)
-
+        // @ used esbuild in development because of hmr failing on tsx files (original tsc for some reason cuts `RefreshSig` variables required for hmr)
+        
         inDevelopment && esbuild({
             // All options are optional
-            include: /\.[t]sx?$/, // default, inferred from `loaders` option
-            // include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+            include: /\.[t]sx?$/, // /\.[jt]sx?$/ - default, inferred from `loaders` option            
             exclude: /node_modules/, // default
             sourceMap: true, // default
-            // minify: process.env.NODE_ENV === 'production',
             minify: false,
-            // target: 'es2017', // default, or 'es20XX', 'esnext'
-            // jsx: 'transform', // default, or 'preserve'
-            // jsxFactory: 'React.createElement',
-            // jsxFragment: 'React.Fragment',
-            // Like @rollup/plugin-replace
-            // define: {
-            //     __VERSION__: '"x.y.z"',
-            // },
             tsconfig: 'tsconfig.json', // default
-            // Add extra loaders
             loaders: {
                 // Add .json files support
                 // require @rollup/plugin-commonjs
-                '.json': 'json',
-                // Enable JSX in .js files too
-                '.js': 'jsx',
+                // '.json': 'json',                
+                '.js': 'jsx',   // Enable JSX in .js files too
             },
         }),
         
