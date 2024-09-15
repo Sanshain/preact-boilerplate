4d93c766898f92ebdd5d60f72e4d4b5169548b03
Alexandr <digital-mag@ya.ru>
2024-09-16 00:33:48 +0300

    Revert "jotai research (failed w/o react-compat) and it takes 45kb instead of 13"
    This reverts commit ff2854ddb050d970da86c404cd6c2ea09a4dd63d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { useEffect } from "preact/hooks";
-
-// import { useAtom } from 'jotai';
-// import Title from './components/Title';
-
-// import { countAtom } from './store/index';
 import style from './style.module.css'
 
 
 const a: string = 'aaa';
 
-
-function App() {
-    
-    // const [count, setCount] = useAtom(countAtom)
-
-    useEffect(() => console.log('effect'), []);
-
+function App () {
     return (
         <div class={style.title}>
             <h1>Hello World, {a}!!</h1>
-            {/* <Title /> */}
             <div className={style.header}>111-111-122</div>
-            {/* <button onClick={e => setCount(v => v + 1)}>increase</button> */}
         </div>
     );
 }
