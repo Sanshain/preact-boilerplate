9d50500f35fff8514b9ee0b5655f11cd85a6722d
Alexandr <digital-mag@ya.ru>
2024-09-15 11:54:05 +0300

    jotai: SyntaxError: Cannot use 'import.meta' outside a module

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import style from './style.module.css'
 
-// import { countAtom } from './store';
-// import { useAtom } from 'jotai'
-// import { Title } from './components/Title';
+import { countAtom } from './store';
+import { useAtom } from 'jotai'
+import { Title } from './components/Title';
 
 
 
 function App() {
     
-    // const [count, setCount] = useAtom(countAtom)
+    const [count, setCount] = useAtom(countAtom)
 
     return (
         <div class={style.title}>
             <h1>Hello World!!!</h1>
-            {/* <Title /> */}
+            <Title />
             <div className={style.header}>111-111-122</div>
-            {/* <button onClick={e => setCount(c => c + 1)}>Increase</button> */}
+            <button onClick={e => setCount(c => c + 1)}>Increase</button>
         </div>
     );
 }
