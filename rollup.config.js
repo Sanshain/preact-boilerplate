b6af658c9b118553b189c5ba84713a8396c4af31
Alexandr <digital-mag@ya.ru>
2024-09-20 08:51:42 +0300

    Revert "a quick example from mobx.js.org is implemented"
    This reverts commit ee1428109dea3a74179f3e15e7cc8cb1461b0e84.

--- rollup.config.js
+++ rollup.config.js
@@ -30,9 +30,6 @@ import linaria from "@linaria/rollup";
 import postcss from 'rollup-plugin-postcss-hot'
 
 
-/// mobx:
-
-import commonjs from "rollup-plugin-commonjs-alternate";
 
 
 import static_files from 'rollup-plugin-static-files';		// copies static files from specific locations (additional optional tool for copying to production folder on prod mode)
@@ -131,16 +128,13 @@ let config = {
             configFile: inDevelopment ? './.dev.babelrc' : './.babelrc' // hmr           
 		}),
 		
-		commonjs(),
         node_resolve({
             extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css']
 		}),
 		
 		/// HMR
 
-		inDevelopment 
-			? prefresh()                                    // hmr
-			: terser()										// pro
+        inDevelopment && prefresh(),                                    // hmr
     ]
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,6 @@ import type { LinariaClassName } from "@linaria/core/types"
 import { modularScale, hiDPI } from 'polished';
 
 
-import { observer } from "mobx-react-lite"
-import { createTimer, } from "./store/index";
-import type { StoreType } from "./store/index";
 
 
 
@@ -51,24 +48,17 @@ function App() {
 
   const link = "https://github.com/Sanshain/preact-boilerplate";
 
-  const MApp = observer(({ timer }: {timer: StoreType}) => 
+  return (
     <div style={{ backgroundColor: 'aliceblue' }}>
       <h1 class={title}>Hello World!!!</h1>
       <p className={style.content}>
         <span>Welcome to </span>
         <a style={{ color: 'green' }} target={"_blank"} href={link}>Sanshain's preact boilerplates</a>
       </p>
-      <hr />
-      <p className={style.content2}>
-        Timer {timer.secondsPassed}
-      </p>
-      <button onClick={e => timer.increase()}>click time</button>
 
       {/* <Title user={"Sasha"} /> */}
     </div>
   );
-
-  return <MApp timer={createTimer()} />
 }
 
 export default App;
